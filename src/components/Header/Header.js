97a7848de609dd8a30e1a2b62f090d1f5d98125f
PRONOY <pronoybanik82@gmail.com>
2022-10-11 20:03:42 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header-container'>
+            <h2>Quiz Total</h2>
+            <div className='header-child'>
+                <Link to='/home'>Home</Link>
+                <Link to='/statistics'>statistics</Link>
+                <Link to='/blog'>blog</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
