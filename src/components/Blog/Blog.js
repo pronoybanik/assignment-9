62abbc0dbcfc3ab74f5b11b0962375d91e57434d
PRONOY <pronoybanik82@gmail.com>
2022-10-12 17:48:49 +0600

    scend quseton

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -5,14 +5,27 @@ const Blog = () => {
         <div>
             
             <div>
-                <h2>what is a perpase react router</h2>
+                <h2>1- What is a perpase react router</h2>
                 <p>
                 React Router is the standard routing
                  library for React. React Router keeps your UI in 
                   sync with the URL
                 </p>
             </div>
-          
+            <div>
+                <h2>2- How dose context api work</h2>
+                <p>The Context API is a React structure that enables you to exchange unique details and assists in solving prop-drilling 
+                    from all levels of your application</p>
+            </div>
+            <div>
+                <div>
+                    <h2>3- What is useref hook</h2>
+                    <p>The useRef Hook allows you to
+                         persist values between renders.
+                          It can be used to store a mutable 
+                          value that doesnot cause a re-render when updated.</p>
+                </div>
+            </div>
             
         </div>
     );
